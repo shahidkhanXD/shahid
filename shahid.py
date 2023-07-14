ee18650149560324ecb94c020ddfda75e346902a
Shahid Khan <84020259+shahidkhanXD@users.noreply.github.com>
2023-07-14 22:42:13 +0500

    Add files via upload

---
+++ shahid.py
@@ -0,0 +1,18 @@
+import os, platform, time, sys
+ 
+try:
+ 
+    import requests
+ 
+except:
+ 
+    os.system('pip install requests')
+ 
+os.system('git pull --quiet 2>/dev/null')
+ 
+bit = platform.architecture()[0]
+ 
+if bit == '64bit':
+    from shahid import main
+ 
+    main()
