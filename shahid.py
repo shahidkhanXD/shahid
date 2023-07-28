962a2bfce5c755de4d85efe9ad820e89745bf0fd
Shahid Khan <84020259+shahidkhanXD@users.noreply.github.com>
2023-07-28 15:34:29 +0500

    Update shahid.py

--- shahid.py
+++ shahid.py
@@ -1,18 +1,1 @@
-import os, platform, time, sys
- 
-try:
- 
-    import requests
- 
-except:
- 
-    os.system('pip install requests')
- 
-os.system('git pull --quiet 2>/dev/null')
- 
-bit = platform.architecture()[0]
- 
-if bit == '64bit':
-    from shahid import menu
- 
-    menu()
+import shahid
